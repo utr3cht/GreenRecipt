73cffa20b403bd2107dcf66ba023f5ff65d106ba
utr3cht <kirito27921@gmail.com>
2025-10-31 11:47:54 +0900

    ユーザー関連のテンプレートファイルを追加し、モデルのフィールドを修正

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class CustomUser(AbstractUser):
     current_points = models.IntegerField(default=0, verbose_name='ポイント')
     rank = models.CharField(max_length=10, default='seed', verbose_name='ランク')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='アカウント更新日時')
-    birthday = models.DateField(verbose_name='生年月日', null=True, blank=True)
+    birthday = models.DateField(verbose_name='生年月日')
     purchased_amount = models.IntegerField(default=0, verbose_name='購入点数')
     lastmonth_point = models.IntegerField(default=0, verbose_name='先月獲得ポイント')
     current_coupons = models.ManyToManyField(
--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.conf import settings
 
+
 class Store(models.Model):
     store_name = models.CharField(max_length=64, verbose_name='店舗名')
     category = models.CharField(max_length=64, verbose_name='カテゴリ')
@@ -16,9 +17,12 @@ class Store(models.Model):
         verbose_name = '加盟店'
         verbose_name_plural = '加盟店'
 
+
 class Receipt(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='ユーザー')
-    store = models.ForeignKey(Store, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='店舗')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, verbose_name='ユーザー')
+    store = models.ForeignKey(
+        Store, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='店舗')
     scanned_at = models.DateField(auto_now_add=True, verbose_name='スキャン日時')
     ocr_text = models.TextField(blank=True, verbose_name='文字起こし内容')
     image_url = models.URLField(max_length=191, verbose_name='画像URL')
@@ -30,9 +34,12 @@ class Receipt(models.Model):
         verbose_name = 'レシート'
         verbose_name_plural = 'レシート'
 
+
 class Inquiry(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='問い合わせユーザー')
-    reply_to_email = models.EmailField(max_length=100, verbose_name='返信先メールアドレス')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
+                             null=True, blank=True, verbose_name='問い合わせユーザー')
+    reply_to_email = models.EmailField(
+        max_length=100, verbose_name='返信先メールアドレス')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='作成日時')
     subject = models.CharField(max_length=100, verbose_name='件名')
     body_text = models.TextField(verbose_name='内容')
@@ -44,12 +51,15 @@ class Inquiry(models.Model):
         verbose_name = 'お問い合わせ'
         verbose_name_plural = 'お問い合わせ'
 
+
 class Coupon(models.Model):
     title = models.CharField(max_length=64, verbose_name='タイトル')
     description = models.CharField(max_length=64, verbose_name='詳細')
     type = models.CharField(max_length=64, verbose_name='タイプ')
-    discount_value = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='割引量')
-    available_stores = models.ManyToManyField(Store, verbose_name='利用可能店舗', blank=True)
+    discount_value = models.DecimalField(
+        max_digits=12, decimal_places=2, verbose_name='割引量')
+    available_stores = models.ManyToManyField(
+        Store, verbose_name='利用可能店舗', blank=True)
 
     def __str__(self):
         return self.title
@@ -58,8 +68,10 @@ class Coupon(models.Model):
         verbose_name = 'クーポン'
         verbose_name_plural = 'クーポン'
 
+
 class Report(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='ユーザー')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, verbose_name='ユーザー')
     generated_at = models.DateTimeField(auto_now_add=True, verbose_name='生成日時')
     description = models.TextField(verbose_name='内容')
 
