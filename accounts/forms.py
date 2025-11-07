63ed1352220c268f052853a900ed19a95563397e
utr3cht <kirito27921@gmail.com>
2025-11-07 10:50:44 +0900

    CustomAuthenticationFormを追加し、ユーザーの役割とメール認証を確認するロジックを実装。ログインフォームのエラーメッセージを改善。CSSスタイルを整理し、レスポンシブデザインを強化。

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 import datetime
+
 
 class CustomUserCreationForm(UserCreationForm):
     birthday = forms.DateField(
@@ -17,7 +19,27 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ('username', 'email', 'birthday',)
 
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = CustomUser
         fields = ('username', 'email', 'birthday',)
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def confirm_login_allowed(self, user):
+        super().confirm_login_allowed(user)
+
+        # 最初に役割をチェック
+        if user.role != 'user':
+            raise forms.ValidationError(
+                "ユーザー名もしくはパスワードが正しくありません。", # ご指定のメッセージに変更
+                code='invalid_role'
+            )
+
+        # 役割が'user'の場合のみ、メール認証をチェック
+        if not user.is_verified:
+            raise forms.ValidationError(
+                "メールアドレスが認証されていません。メールをご確認ください。",
+                code='unverified_email'
+            )
